3ecedb3c1c3c674cec6b6bbf05a959173bce7e4a
Elias Häußler <mail@elias-haeussler.de>
2017-03-21 12:16:41 +0100

    Added database and database connection

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class Database {
+
+	// Class instance
+	private static Database instance;
+
+	// Database name
+	private static string name = "beatlux";
+
+	// Database extension
+	private static string ext = "db";
+
+	// Database connection
+	private static IDbConnection con;
+
+
+
+	private Database ()
+	{
+		// Path to database
+		string uri = "URI=file:" + Application.dataPath + "/" + name + "." + ext;
+
+		// Connect to database
+		con = connect(uri);
+	}
+
+	private IDbConnection connect(string uri)
+	{
+		// Connect to database
+		con = (IDbConnection) new SqliteConnection (uri);
+
+		// Open database connection
+		con.Open();
+
+		return con;
+	}
+
+
+
+	// Get instance (Singleton)
+	public static IDbConnection getConnection ()
+	{
+		// Create instance if not exists
+		if (instance == null) {
+			instance = new Database ();
+		}
+
+		// Return database connection
+		return con;
+	}
+}
